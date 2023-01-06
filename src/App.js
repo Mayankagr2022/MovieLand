2696e064ac6ef9b09f21573ead2a98e9ef024d4b
Mayank Agarwal <arenmayank20@gmail.com>
2023-01-06 10:00:32 +0530

    Updated Movie Card

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-// import './App.css';
-// import {useState , useEffect} from 'react';
-
-// const App = () =>  {
-
-//   const [counter, setCounter] = useState(0);
-  
-//   useEffect(() => {
-//     setCounter(0);
-//   },[]);
-//   return (
-  
-//   <div className="App">
-    
-//      <button onClick={() => setCounter((prevCount) => prevCount - 1 ) }>-</button>
-
-//      <h1>{counter}</h1>
-    
-//      <button onClick={() => setCounter((prevCounter) => prevCounter + 1 )}>+</button>
-    
-//     </div>
-//   );
-// }
-
-// export default App;
 import React, { useState, useEffect } from "react";
 
 import MovieCard from "./MovieCard";
